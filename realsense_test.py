bd9026e90395020f1712c3a747322f8a12f47d44
engrjimmy <eng.jimmy.majumder@gmail.com>
2023-05-05 00:31:01 +0900

    file modified realsense_test.py

--- realsense_test.py
+++ realsense_test.py
@@ -5,8 +5,9 @@ import pyrealsense2 as rs
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+#config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
@@ -32,7 +33,7 @@ try:
 
         # Show images
         cv2.imshow('RealSense D435', images)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord ("Z"):
             break
 
 finally:
